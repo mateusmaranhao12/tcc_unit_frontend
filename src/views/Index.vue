0b5a1b3abe1b74e797d64e1db159d01564a1a07c
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-02-14 10:14:30 -0300

    Init project

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-3xl font-bold">
+    Hello world!
+  </h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+  },
+})
+export default class Index extends Vue { }
+</script>
