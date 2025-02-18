174bde4ea029b05c463055dcc0929d86d9a25613
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-02-17 23:07:11 -0300

    Update NavbarMobile

--- src/components/NavbarIndex.vue
+++ src/components/NavbarIndex.vue
@@ -35,56 +35,23 @@
         <router-link to="/cadastro"
           class="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition">Cadastre-se</router-link>
       </div>
-
-      <!-- Menu Hamburguer para telas menores -->
-      <button class="md:hidden text-gray-800 focus:outline-none" @click="toggleMenu">
-        <i class="fa-solid fa-bars text-2xl"></i>
-      </button>
     </div>
 
     <!-- Mobile Menu -->
-    <ul :class="{ 'hidden': !menuOpen, 'block': menuOpen }"
-      class="md:hidden bg-white w-full shadow-md text-center py-4">
-      <li>
-        <router-link to="/" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
-          <i class="fa-solid fa-house"></i> Home
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/sobre" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
-          <i class="fa-solid fa-circle-info"></i> Sobre
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/medicos" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
-          <i class="fa-solid fa-user-doctor"></i> Médicos
-        </router-link>
-      </li>
-      <li>
-        <router-link to="/login" class="text-gray-700 text-lg font-semibold block py-2"
-          @click="toggleMenu">Login</router-link>
-      </li>
-      <li>
-        <router-link to="/cadastro"
-          class="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition block mx-auto mt-2 w-40"
-          @click="toggleMenu">Cadastre-se</router-link>
-      </li>
-    </ul>
+    <NavbarMobile />
   </nav>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import NavbarMobile from './NavbarMobile.vue'
 
-@Options({})
-export default class NavbarIndex extends Vue {
-
-  menuOpen = false
-
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen
+@Options({
+  components: {
+    NavbarMobile
   }
-
+})
+export default class NavbarIndex extends Vue {
 }
 </script>
 
---
+++ src/components/NavbarMobile.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- Menu Hamburguer para telas menores -->
+    <button class="md:hidden text-gray-800 focus:outline-none" @click="toggleMenu">
+        <i class="fa-solid fa-bars text-2xl"></i>
+    </button>
+
+    <!-- Mobile Menu -->
+    <ul :class="{ 'hidden': !menuOpen, 'block': menuOpen }"
+        class="md:hidden bg-white w-full shadow-md text-center py-4">
+        <li>
+            <router-link to="/" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
+                <i class="fa-solid fa-house"></i> Home
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/sobre" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
+                <i class="fa-solid fa-circle-info"></i> Sobre
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/medicos" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
+                <i class="fa-solid fa-user-doctor"></i> Médicos
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/login" class="text-gray-700 text-lg font-semibold block py-2" @click="toggleMenu">
+                Login
+            </router-link>
+        </li>
+        <li>
+            <router-link to="/cadastro"
+                class="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition block mx-auto mt-2 w-40"
+                @click="toggleMenu">
+                Cadastre-se
+            </router-link>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class NavbarMobile extends Vue {
+    menuOpen = false
+
+    toggleMenu() {
+        this.menuOpen = !this.menuOpen
+    }
+}
+</script>
